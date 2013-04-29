5d7a6ebfab5fc429e7657e8190b571dfa30cfc63
Guilhem Lettron <guilhem@lettron.fr>
2013-04-29 12:34:56 +0200

    use libraries

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,17 +18,6 @@
 # limitations under the License.
 #
 
-def which(cmd)
-  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-    exts.each { |ext|
-      exe = "#{path}/#{cmd}#{ext}"
-      return exe if File.executable? exe
-    }
-  end
-  return nil
-end
-
 unless which("htpasswd")
 	include_recipe "htpasswd::build-in"
 end
