3f93587b025c2d200cb6eff1103235d8ad39d999
Grant Ridder <shortdudey123@gmail.com>
2017-04-01 18:10:02 -0700

    Fix FC052, FC064, FC065, FC066

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,6 @@ license 'Apache 2.0'
 description 'Provider for htpasswd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.5'
-suggests 'python'
 
 %w(
   centos
@@ -18,3 +17,7 @@ suggests 'python'
 end
 
 depends 'python'
+
+source_url 'https://github.com/redguide/htpasswd' if respond_to?(:source_url)
+issues_url 'https://github.com/redguide/htpasswd/issues' if respond_to?(:issues_url)
+chef_version '>= 11.0' if respond_to?(:chef_version)
