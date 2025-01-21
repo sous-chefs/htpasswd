7e66bad4d532aa7d700df18d4f2c07a6fca39398
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2025-01-21 14:00:48 -0800

    Fix passing new_resource properties to helpers
    Don't use instance variables; just pass in new_resource directly.
    
    Signed-off-by: ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>

--- resources/default.rb
+++ resources/default.rb
@@ -13,17 +13,17 @@ property :type, String,
 property :mode, String, default: '0640'
 
 action :add do
-  unless htpasswd_user_set?(@new_resource)
+  unless htpasswd_user_set?(new_resource)
     if ::File.exist?(new_resource.file)
-      ruby_block "Add user #{@new_resource.user} in #{@new_resource.name}" do
+      ruby_block "Add user #{new_resource.user} in #{new_resource.name}" do
         block do
-          htpasswd_add(@new_resource)
+          htpasswd_add(new_resource)
         end
       end
     else
-      ruby_block "Create user #{@new_resource.user} in #{@new_resource.name}" do
+      ruby_block "Create user #{new_resource.user} in #{new_resource.name}" do
         block do
-          htpasswd_create(@new_resource)
+          htpasswd_create(new_resource)
         end
       end
     end
@@ -32,16 +32,16 @@ action :add do
 end
 
 action :overwrite do
-  converge_by("Overwrite file #{@new_resource.name} with user #{@new_resource.user}") do
-    htpasswd_create(@new_resource)
+  converge_by("Overwrite file #{new_resource.name} with user #{new_resource.user}") do
+    htpasswd_create(new_resource)
   end
   fix_perms(new_resource)
 end
 
 action :delete do
-  if htpasswd_user_exists?(@new_resource)
-    converge_by("Delete user #{@new_resource.user} in #{@new_resource.name}") do
-      htpasswd_delete(@new_resource)
+  if htpasswd_user_exists?(new_resource)
+    converge_by("Delete user #{new_resource.user} in #{new_resource.name}") do
+      htpasswd_delete(new_resource)
     end
   end
   fix_perms(new_resource)
